53dc218d9dafac57d5d75735669f558560cc475e
Usu2 <Usu2@example.com>
2020-05-19 01:26:06 +0200

    Valido la clase1 en rama cambios

---
+++ actividad4/src/clase1.java
@@ -0,0 +1,10 @@
+
+public class clase1 {
+
+	public static void main(String[] args) {
+		System.out.println(" - - - Mensaje clase1 - - - - ");
+		System.out.println(" - - - Mensaje clase1 - - - - ");
+
+	}
+
+}
